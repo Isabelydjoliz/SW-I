470bc2a6c870028b26d9021d2faaadd62c0dbccd
Isabelydjoliz <124791216+Isabelydjoliz@users.noreply.github.com>
2024-02-23 12:39:35 -0300

    Add files via upload

---
+++ Exemplo/Lampada/Lampada/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Lampada
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            // INSTÂNCIA DE UM OBJETO A PARTIR DE UMA CLASSE EXISTENTE
+            Lampada lampada1 = new Lampada();
+            Lampada lampada2 = new Lampada();
+
+            //ACESSANDO UM ATRIBUTI PÚBLICO DO OBJETO
+            lampada1.Ligada = true;
+            lampada1.Potencia = 150;
+            lampada1.Cor = "Vermelha";
+
+            lampada2.Ligada = false;
+            lampada2.Potencia = 100;
+            lampada2.Cor = "Azul";
+
+            //MOSTRANDO NA TELA
+            Console.WriteLine("ESSA É A LÂMPADA 1");
+            Console.WriteLine("A lâmpada está ligada? " + lampada1.Ligada);
+            Console.WriteLine("A lâmpada tem qual potência? " + lampada1.Potencia);
+            Console.WriteLine("A lâmpada é de qual cor? " + lampada1.Cor);
+            Console.WriteLine("************************************************");
+
+            Console.WriteLine("ESSA É A LÂMPADA 2");
+            Console.WriteLine("A lâmpada está ligada? " + lampada2.Ligada);
+            Console.WriteLine("A lâmpada tem qual potência? " + lampada2.Potencia);
+            Console.WriteLine("A lâmpada é de qual cor? " + lampada2.Cor);
+
+
+            Console.WriteLine("Cor da lâmpada 1: " + lampada1.Cor);
+            Console.WriteLine("Cor da lâmpada 1: " + lampada1.Cor);
+
+
+        }
+    }
+}
