95f34d18a792ef0467e39bd6cf67f9b4b7dcc496
Isabelydjoliz <124791216+Isabelydjoliz@users.noreply.github.com>
2024-02-29 12:25:28 -0300

    Add files via upload

---
+++ Lampada/Lampada/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lampada
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            // INSTÂNCIA DE UM OBJETO A PARTIR DE UMA CLASSE EXISTENTE
+            Lampada lampada1 = new Lampada();
+
+            //chmando o método para ligar a lamp1
+            lampada1.ligar();
+
+            //chamando o método para verificar se a lamp1 está ligada
+            Console.WriteLine("a lâmpada está ligada? " + lampada1.estaligada());
+
+
+        }
+    }
+}
