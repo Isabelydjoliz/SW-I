9b3f2c5e62887a7fd057b7cf92d22c56d5db6c8f
Isabelydjoliz <124791216+Isabelydjoliz@users.noreply.github.com>
2024-04-12 12:35:22 -0300

    Add files via upload

---
+++ exercicios_01/AreaQuadradoRetangulo/AreaQuadradoRetangulo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AreaQuadradoRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcule a área do quadrado/retângulo");
+
+            Area xyz = new Area();
+
+            Console.WriteLine(xyz.Calcular());
+
+            Console.WriteLine(xyz.Dados());
+        }
+    }
+}
