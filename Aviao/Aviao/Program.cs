8ea074e8effd202317d2abed08a16faf2ddf820f
Isabelydjoliz <124791216+Isabelydjoliz@users.noreply.github.com>
2024-04-11 21:22:25 -0300

    Add files via upload

---
+++ Aviao/Aviao/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Aviao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controle Aviao = new Controle();
+
+            Aviao.Compania = "Gol";
+            Aviao.Modelo = "Boing2411";
+            Aviao.Acelerar(40);
+            Aviao.Cima(19000);
+
+            Console.WriteLine($"O nosso avião está a: " + Aviao.Altura + "pés, a velocidade atual é de: " + Aviao.Velocidade + "km/h. Nós somos da " + Aviao.Compania + ", e o nosso modelo atual é: " + Aviao.Modelo);
+        }
+    }
+}
