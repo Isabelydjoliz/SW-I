e138b82f1b79bc7802e58738e43994b6addcabb5
Isabelydjoliz <124791216+Isabelydjoliz@users.noreply.github.com>
2024-04-12 12:31:24 -0300

    Add files via upload

---
+++ exercicios_01/AreaQuadradoRetangulo/Fotos/Fotos/Fotos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício3_Album
+{
+    internal class Fotos
+    {
+        public double NumeroFotos { get; set; }
+        public double FotosPagina { get; set; }
+        public double Restante { get; set; }
+
+        public double Div { get; set; }
+
+        public string NumeroDePaginas()
+        {
+
+            Console.WriteLine("Digite quantas fotos você vai usar");
+            NumeroFotos = Convert.ToDouble(Console.ReadLine());
+            FotosPagina = 2;
+
+            Restante = NumeroFotos % FotosPagina;
+            Div = (NumeroFotos / FotosPagina) - 0.5;
+
+            if (Restante == 1)
+            {
+                return "Com duas fotos por página você terá " + Div + " páginas com fotos, e irá sobrar " + Restante + " fotos";
+            }
+            else
+            {
+                return "Com duas fotos por página você terá " + NumeroFotos / FotosPagina + " páginas com fotos";
+            }
+        }
+    }
+}
---
+++ exercicios_01/AreaQuadradoRetangulo/Fotos/Fotos/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercício3_Album
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculando número de fotos por página");
+            Console.WriteLine("");
+
+            Fotos album = new Fotos();
+
+            Console.WriteLine(album.NumeroDePaginas());
+
+        }
+    }
+}
